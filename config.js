07db89e8dfe24a86dd11accb31ddd0c70ec8b5f5
Nadeenpoorna-lk <nadeenpoorna058@gmail.com>
2025-01-26 16:34:30 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,10 +5,10 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "YsYlRQaa#heVle58By3R4QweJmw8AdzaAAd8wFawfToUY8mKvfcE", //put your session id
+SESSION_ID: process.env.SESSION_ID || "", //put your session id
 MONGODB: process.env.MONGODB || "mongodb+srv://sam:sam@cluster0.u1smxsv.mongodb.net/?retryWrites=true&w=majority",
 MODE: process.env.MODE || "private", //private | public | groups | inbox
-STATUES_REPLY: process.env.STATUES_REPLY || "true", //true or false
+STATUES_REPLY: process.env.STATUES_REPLY || "false", //true or false
 AUTO_REACT: process.env.AUTO_REACT || "true", //true or false
 INBOX_BLOCK: process.env.INBOX_BLOCK || "false", //true or false
 READ_MESSAGE: process.env.READ_MESSAGE || "false", //true or false
